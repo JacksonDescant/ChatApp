ad3971e9df24b0332f479e4e06f513d3487199e8
Jackson Descant <jacksondescant@gmail.com>
2024-05-24 13:54:03 -0500

    working signalr

--- ChatApp.Server/Program.cs
+++ ChatApp.Server/Program.cs
@@ -7,9 +7,19 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSignalR();
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseCors();
 app.UseDeveloperExceptionPage();
 
 
